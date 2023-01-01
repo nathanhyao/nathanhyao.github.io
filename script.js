64ff98ae0a4ec5fcbab8c582ce857ebe8159a386
nathanhyao <nathanhyao@gmail.com>
2023-01-01 03:19:55 -0500

    responsive navbar complete, colored section dividers

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(window).scroll(function () {
   var winScroll = $(this).scrollTop();
-  console.log(winScroll);
+  // console.log(winScroll);
 
   $(".background").css({
     "transform": "translate(0, " + winScroll / 20 + "%)"
@@ -11,7 +11,16 @@ $(window).scroll(function () {
 })
 
 // Navigation hamburger menu on/off
-$(".toggle-button").click(function (event) {
-  $("nav").toggleClass("active");
-  event.preventDefault();
+$(".mobile-nav-toggle").click(function (event) {
+  const visibility = $(".nav-links").attr("data-visible")
+  console.log(visibility);
+
+  if (visibility == "false") {
+    $(".nav-links").attr("data-visible", "true");
+    $(".mobile-nav-toggle").attr("aria-expanded", "true");
+  }
+  else if (visibility == "true") {
+    $(".nav-links").attr("data-visible", "false");
+    $(".mobile-nav-toggle").attr("aria-expanded", "false");
+  }
 })
